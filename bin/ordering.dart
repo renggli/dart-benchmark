b98e71d1f8400e205314389ffb2b595ac974fe34
Lukas Renggli <renggli@gmail.com>
2022-10-02 15:37:01 +0200

    Initial commit

---
+++ bin/ordering.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+
+import 'package:benchmark/benchmark.dart';
+import 'package:collection/collection.dart';
+import 'package:more/comparator.dart';
+import 'package:more/ordering.dart';
+
+final random = Random(42);
+final values = List.generate(1000, (i) => 1000.0 * random.nextDouble())..sort();
+final tests = List.generate(1000, (i) => 1000.0 * random.nextDouble())
+    .followedBy(values)
+    .toList();
+
+void packageCollection() {
+  int doubleCompare(double a, double b) => a.compareTo(b);
+  for (var test in tests) {
+    values.binarySearch(test, doubleCompare);
+  }
+}
+
+void packageOrdering() {
+  final ordering = Ordering.natural<double>();
+  for (var test in tests) {
+    ordering.binarySearch(values, test);
+  }
+}
+
+void packageComparable() {
+  final comparator = naturalComparator<double>();
+  for (var test in tests) {
+    comparator.binarySearch(values, test);
+  }
+}
+
+void main() {
+  experiments(
+    control: packageCollection,
+    experiments: {
+      'Ordering': packageOrdering,
+      'Comparable': packageComparable,
+    },
+  );
+}
---
+++ lib/benchmark.dart
@@ -0,0 +1,91 @@
+import 'dart:io';
+
+import 'package:data/stats.dart';
+import 'package:more/more.dart';
+
+typedef Benchmark = void Function();
+
+const defaultWarmup = Duration(milliseconds: 200);
+const defaultMeasure = Duration(milliseconds: 1000);
+const defaultSamples = 25;
+
+/// Compares the execution time of [control] vs [experiment].
+void experiments({
+  required Benchmark control,
+  required Map<String, Benchmark> experiments,
+  Duration warmup = defaultWarmup,
+  Duration measure = defaultMeasure,
+  int samples = defaultSamples,
+}) {
+  stdout.write('Control'.padRight(20));
+  final controlSamples =
+      benchmark(control, warmup: warmup, measure: measure, samples: samples);
+  final controlJackknife =
+      Jackknife<double>(controlSamples, (list) => list.arithmeticMean());
+  stdout.writeln(result(controlJackknife, unit: 'μs'));
+
+  for (final name in experiments.keys) {
+    stdout.write(name.padRight(20));
+    final experimentSamples = benchmark(experiments[name]!,
+        warmup: warmup, measure: measure, samples: samples);
+    final experimentJackknife =
+        Jackknife<double>(experimentSamples, (list) => list.arithmeticMean());
+    stdout.writeln(result(experimentJackknife, unit: 'μs'));
+
+    stdout.write(' '.padRight(20));
+    final percentChangeSamples = List.generate(
+        samples,
+        (i) =>
+            (100.0 *
+                (controlSamples[i] - experimentSamples[i]) /
+                controlSamples[i]) -
+            100.0);
+    final percentChangeJackknife = Jackknife<double>(
+        percentChangeSamples, (list) => list.arithmeticMean());
+    stdout.writeln(result(percentChangeJackknife, unit: '%'));
+  }
+}
+
+String result(Jackknife<double> jackknife,
+    {int precision = 3, required String unit}) {
+  final printer = FixedNumberPrinter(precision: precision).after(unit);
+  return '${printer(jackknife.estimate)} '
+      '[${printer(jackknife.lowerBound)}; '
+      '${printer(jackknife.upperBound)}]';
+}
+
+/// Measures the time it takes to run [function] in microseconds.
+///
+/// It does so in two steps:
+///
+///  - the code is warmed up for the duration of [warmup]; and
+///  - the code is benchmarked for the duration of [measure].
+///
+/// The resulting duration is the average time measured to run [function] once.
+List<double> benchmark(
+  Benchmark function, {
+  Duration warmup = defaultWarmup,
+  Duration measure = defaultMeasure,
+  int samples = defaultSamples,
+}) {
+  _benchmark(function, warmup);
+  final benchmarkSamples = <double>[];
+  for (var i = 0; i < samples; i++) {
+    benchmarkSamples.add(_benchmark(function, measure));
+  }
+  return benchmarkSamples;
+}
+
+double _benchmark(Benchmark function, Duration duration) {
+  final watch = Stopwatch();
+  final micros = duration.inMicroseconds;
+  var count = 0;
+  var elapsed = 0;
+  watch.start();
+  while (elapsed < micros) {
+    function();
+    elapsed = watch.elapsedMicroseconds;
+    count++;
+  }
+  return elapsed / count;
+}
